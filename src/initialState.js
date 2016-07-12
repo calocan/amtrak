be92d3656fad0d8ca1b51f091c6aed4ab6fb0619
calocan <andy@calthorpeanalytics.com>
2016-07-12 15:28:41 +0200

    Still filling out model data

--- src/initialState.js
+++ src/initialState.js
@@ -126,48 +126,108 @@ export default Map({
             'Seat Comfort (Metro)': Map({
                 status: Statuses.INITIALIZED,
                 id: 'f3ad4189-7150-4048-a4c9-c3e9652e9482',
-                anchorId: 'id.v297r7jcxci4',
+                anchorId: 'id.em6st57x7wbe',
                 scenes: Map({
-                    'Forward-facing Seats on AMTRAK': Map({
+                    'Empty Rows': Map({
+                        anchorId: 'id.em6st57x7wbe'
+                    }),
+                    'Plush Seats': Map({
+                        anchorId: 'id.rcz0hdr6gctf'
+                    }),
+                    'Occasional Wooden Seats': Map({
+                        anchorId: 'id.nj75dz9x9en1'
+                    }),
+                    'Classic Wooden Seats': Map({
+                        anchorId: 'id.xhr56xmk5mgb'
+                    }),
+                    'Seat Height': Map({
                         anchorId: 'id.v297r7jcxci4'
                     }),
-                    "AMTRAK's Sightseeing Seats": Map({
-                        anchorId: 'id.17g6w82r9xg0'
-                    }),
-                    'Consolidation of Close Stops': Map({
-                        anchorId: 'id.jkjm80gwudui'
-                    }),
-                    'Tram Upgrade': Map({
-                        anchorId: 'id.vwedkoe7xxns'
-                    }),
-                    'Consolidation Challenges': Map({
-                        anchorId: 'id.ifjl8mnx6p4n'
-                    }),
+                    'Armrests and Recliners': Map({
+                        anchorId: 'id.bbssm7d9ycql'
+                    })
                 })
             }),
 
             'Seat Comfort (AMTRAK/Bus)': Map({
                 status: Statuses.INITIALIZED,
                 id: '843cbe82-5a4a-4453-9766-488049133e9d',
-                anchorId: 'id.v297r7jcxci4',
+                anchorId: 'id.53cvna734bl4',
                 scenes: Map({
                     'Forward-facing Seats on AMTRAK': Map({
-                        anchorId: 'id.v297r7jcxci4'
+                        anchorId: 'id.53cvna734bl4'
                     }),
                     "AMTRAK's Sightseeing Seats": Map({
-                        anchorId: 'id.17g6w82r9xg0'
+                        anchorId: 'id.nups2saj5zpa'
                     }),
-                    'Consolidation of Close Stops': Map({
-                        anchorId: 'id.jkjm80gwudui'
-                    }),
-                    'Tram Upgrade': Map({
-                        anchorId: 'id.vwedkoe7xxns'
-                    }),
-                    'Consolidation Challenges': Map({
-                        anchorId: 'id.ifjl8mnx6p4n'
+                    'Low-floor buses have rear-facing seats': Map({
+                        anchorId: 'id.s9a7cdqojj39'
                     }),
                 })
-            })
+            }),
+            
+            'Personal Space and Privacy': Map({
+                status: Statuses.INITIALIZED,
+                id: '268c95fd-8a33-4636-bf72-ceaff2c1e997',
+                anchorId: 'id.n0mwt01mtot1',
+                scenes: Map({
+                    'Train Compartment': Map({
+                        anchorId: 'id.n0mwt01mtot1'
+                    }),
+                    'AMTRAK Roomettes': Map({
+                        anchorId: 'id.ciyi25f3rtt'
+                    }),
+                    'AMTRAK Bedrooms': Map({
+                        anchorId: 'id.6hku2y24we4a'
+                    }),
+                    'Private Metro Compartments': Map({
+                        anchorId: 'id.cf2ldt4sfy35'
+                    }),
+                })
+            }),
+
+            'Personal Space and Privacy': Map({
+                status: Statuses.INITIALIZED,
+                id: '268c95fd-8a33-4636-bf72-ceaff2c1e997',
+                anchorId: 'id.c784pbnlt1sa',
+                scenes: Map({
+                    'AMTRAK Toilet': Map({
+                        anchorId: 'id.c784pbnlt1sa'
+                    }),
+                    'Metro Toilet': Map({
+                        anchorId: 'id.wkg1zdsmjr7b'
+                    }),
+                    'Tram Toilet': Map({
+                        anchorId: 'id.tffs1gaxh5t5'
+                    }),
+                    'Metro Café': Map({
+                        anchorId: 'id.dfk0f1h01dbd'
+                    }),
+                    'Tram Café': Map({
+                        anchorId: 'id.5pgtgqe6khzj'
+                    })
+                })
+            }),
+
+            'Metro Station With Faregates': Map({
+                status: Statuses.INITIALIZED,
+                id: '605a8f98-af02-4b26-8145-cfe247f91dba',
+                anchorId: 'id.97xvu0r03y0e',
+                scenes: Map({
+                    'Metro Entrance with Faregates': Map({
+                        anchorId: 'id.97xvu0r03y0e'
+                    }),
+                    'Barriers Waste Time and Space': Map({
+                        anchorId: 'id.z94jy3r4mx0n'
+                    }),
+                    'Amenities Must Be Inside or Outside Faregates': Map({
+                        anchorId: 'id.tffs1gaxh5t5'
+                    }),
+                    'Accessing Transit is Cumbersome': Map({
+                        anchorId: 'id.dfk0f1h01dbd'
+                    }),
+                })
+            }),
         })
     })
 })
